8e3e2b5cb7330cf36e1b5c3f48890b3db5d33199
aniket umbc <aniket.umbc@gmail.com>
2017-11-06 16:59:04 +0530

    add Http module and observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { User} from './shared/module/user';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  users: User[];
+
+
+  constructor (private http: Http) {}
+ngOnInit() {
+//  HTTP call for grab users and subscribe them observable 
+this.http.get('https://reqres.in/api/users' )
+.subscribe(data => {
+  console.log(data.json());
+  this.users = data.json().data;
+});
+
+}
+
+
 }
